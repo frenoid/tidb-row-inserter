c99e153e6a95dcb26fc58c35d7a571bd81543ffa
Lim Xing Kang Norman <norman.lim@Lims-MacBook-Pro.local>
2022-06-21 18:55:47 +0800

    feat. parameterize the no of rows inserted

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
+NO_OF_ROWS = int(os.getenv("NO_OF_ROWS", 100))
 
 def get_name(char_count: int) -> str:
   ran = ''.join(random.choices(string.ascii_uppercase, k = char_count))    
@@ -26,7 +27,7 @@ if __name__ == "__main__":
   query = "INSERT INTO test_table (first_name, last_name) VALUES (%s, %s);"
 
   cur = cnx.cursor()
-  for i in range(100):
+  for i in range(NO_OF_ROWS):
     cur.execute(query, (get_name(12), get_name(8)))
     cnx.commit()
     print("INSERTED", i+1, "row")
