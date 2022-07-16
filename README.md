c584ac263d95211deb141896a55f6843601d4b2b
Norman Lim <frenoid@gmail.com>
2022-07-16 15:29:16 +0800

    add new write mode: CONTIONUS

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # TiDB Row Inserter
 Simple python script that inserts rows into TiDB. Useful for benchmarking TiDB's CDC performance
 
-Comes with [Dockerfile](./Dockerfile) and [K8s CronJob Manifest](./k8s/CronJob.yaml)!
+It has 2 write modes
+1. SINGLE which writes once and exits
+2. CONTINOUS which writes as long as the process lives
+
+Comes with [Dockerfile](./Dockerfile) and 2 manifests
+1. [K8s CronJob Manifest](./k8s/CronJob.yaml) suited for the SINGLE write mode
+2. [K8s Deployment Manifest](./k8s/Deployment.yaml) suited for the DEPLOYMENT write mode
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import mysql.connector
 import os
 import string
 import random
+from time import sleep
 
 
 MYSQL_HOST = os.getenv("MYSQL_HOST")
@@ -10,6 +11,8 @@ MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
 NO_OF_ROWS = int(os.getenv("NO_OF_ROWS", 100))
+WRITE_MODE = os.getenv("WRITE_MODE", "SINGLE")
+WRITE_INTERVAL = os.getenv("WRITE_INTERVAL", 5)
 
 def get_name(char_count: int) -> str:
   ran = ''.join(random.choices(string.ascii_uppercase, k = char_count))    
@@ -18,6 +21,7 @@ def get_name(char_count: int) -> str:
 
 if __name__ == "__main__":
   print("START")
+  print("WRITE MODE:", WRITE_MODE)
   print(MYSQL_HOST)
   print("WRITING TO " + MYSQL_DATABASE + "." + "test_table")
   cnx = mysql.connector.connect(user=MYSQL_USER,
@@ -27,11 +31,17 @@ if __name__ == "__main__":
                               database=MYSQL_DATABASE)
   query = "INSERT INTO test_table (first_name, last_name) VALUES (%s, %s);"
 
-  cur = cnx.cursor()
-  for i in range(NO_OF_ROWS):
-    cur.execute(query, (get_name(12), get_name(8)))
-    cnx.commit()
-    print("INSERTED", i+1, "row")
+
+  while True:
+    cur = cnx.cursor()
+    for i in range(NO_OF_ROWS):
+      cur.execute(query, (get_name(12), get_name(8)))
+      cnx.commit()
+      print("INSERTED", i+1, "row")
+    if WRITE_MODE == "SINGLE":
+      print("TERMINATE SINGLE WRITE")
+      break
+    sleep(int(WRITE_INTERVAL))
 
   cnx.close()
   print("FINISH")
